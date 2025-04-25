b7aede872cfdd6d598cf49d0359d74f555ff1fef
Chester Gould <chester@chesters-desktop>
2025-04-25 00:55:35 -0700

    stats and thesis stuff

--- stats/app/Main.hs
+++ stats/app/Main.hs
@@ -1,19 +1,22 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, BlockArguments #-}
 module Main (main) where
 
 import Data.Aeson
 import Data.Aeson.Types
+import Data.Bifunctor
 import Data.String
-import Data.Vector
+import Data.Vector hiding ((++), map)
+import Statistics.Distribution.StudentT
 import Statistics.Test.StudentT
+import Statistics.Test.Types
+import Statistics.Types
 import System.Environment
 
 data BenchTimes = BenchTimes (Vector Double)
   deriving Show
 
-parseObjectPath :: (Value -> Parser a) -> [Key] -> Value -> Parser a
-parseObjectPath p [] v = p v
-parseObjectPath p (k : ks) v = withObject (show k) (\o -> explicitParseField (parseObjectPath p ks) o k) v
+data BenchResult = BenchResult { name :: String, result :: TestResult, test :: Test StudentT }
+  deriving Show
 
 instance FromJSON BenchTimes where
   parseJSON v = (BenchTimes . fromList) <$>
@@ -30,32 +33,44 @@ resultFiles =
   [ "asymptotics.stt.json"
   , "conv_eval.stt.json"
   , "stlc.stt.json"
-  , "stlc100k.stt.json"
-  , "stlc10k.stt.json"
+  -- , "stlc100k.stt.json" -- DNF
+  -- , "stlc10k.stt.json" -- DNF
   , "stlc5k.stt.json"
   , "stlc_lessimpl.stt.json"
-  , "stlc_lessimpl10k.stt.json"
+  -- , "stlc_lessimpl10k.stt.json" -- DNF
   , "stlc_lessimpl5k.stt.json"
   , "stlc_small.stt.json"
   , "stlc_small10k.stt.json"
   , "stlc_small5k.stt.json"
   ]
 
+analyzeBench :: FilePath -> FilePath -> FilePath -> IO (Either String BenchResult)
+analyzeBench unmodDir modDir resFile = do
+  unmodContent <- readFile (unmodDir ++ "/" ++ resFile)
+  modContent <- readFile (modDir ++ "/" ++ resFile)
+  return $ first ((resFile ++ ": ") ++) do
+    (BenchTimes unmodTimes) <- eitherDecode (fromString unmodContent)
+    (BenchTimes modTimes) <- eitherDecode (fromString modContent)
+    let test = BGreater -- We want to know if the modified smalltt is slower than the unmodified
+    let pValue = mkPValue 0.01 -- Significance level of 0.01
+    case welchTTest test unmodTimes modTimes of
+      Nothing -> Left "welchTTest error"
+      Just res -> return (BenchResult resFile (isSignificant (testSignificance res) res) res)
 
 -- unmodified, modified directories
-dataAnalysis :: FilePath -> FilePath -> Maybe a
+dataAnalysis :: FilePath -> FilePath -> IO (Either String [BenchResult])
+dataAnalysis unmodDir modDir = sequenceA <$> sequenceA (map (analyzeBench unmodDir modDir) resultFiles)
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [path] -> do
-      contents <- readFile path
-      let
-        x :: Either String BenchTimes
-        x = eitherDecode (fromString contents) 
-      putStrLn (show x)
+    [unmodDir, modDir] -> do
+      res <- dataAnalysis unmodDir modDir
+      putStrLn (show res)
     _ -> usage
 
 usage :: IO ()
-usage = putStrLn "usage"
+usage = do
+  progName <- getProgName
+  putStrLn ("usage: " ++ progName ++ " <unmodified directory> <modified directory>")
